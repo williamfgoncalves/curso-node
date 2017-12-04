a27135585721f2990329d89cde12e48c6d7ea5b8
William Goncalves <william.goncalves@cwi.com.br>
2017-12-04 01:25:59 -0200

    feat: add CRUD of products

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -3,33 +3,91 @@
 const mongoose = require('mongoose'); 
 const Product = mongoose.model('Product'); 
 const validate = require("validate.js"); 
+const modelProduct = 'title price slug tags'
+
+exports.get = (req, res, next) =>  {
+    Product
+        .find({ active:true },
+                modelProduct)
+            .then(data =>  {
+                res.status(200).send(data ); 
+            }).catch(e =>  {
+                res.status(400).send(e ); 
+            }); 
+}; 
+
+exports.getBySlug = (req, res, next) =>  {
+    Product
+        .find({ slug: req.params.slug,
+                active: true
+            },  modelProduct)
+            .then(data =>  {
+                res.status(200).send(data ); 
+            }).catch(e =>  {
+                res.status(400).send(e ); 
+            }); 
+};
+
+exports.getByTags = (req, res, next) =>  {
+    Product
+        .find({ tags: req.params.tags,
+                active: true
+            },  modelProduct)
+            .then(data =>  {
+                res.status(200).send(data ); 
+            }).catch(e =>  {
+                res.status(400).send(e ); 
+            }); 
+};
 
 exports.post = (req, res, next) =>  {
 
-    var product = new Product(req.body);
+    var product = new Product(req.body); 
 
-    if (!valiteFieldsProduct(product)) {
-        res.status(400).send({message: 'Preencha os campos corretamente!'}); 
+    if ( ! valiteFieldsProduct(product)) {
+        res.status(400).send( {message:'Preencha os campos corretamente!'}); 
     }
     
     product.save()
             .then(() =>  {
-                res.status(201).send({message: 'Your product has been successfully saved!'}); 
+                res.status(201).send( {message:'Your product has been successfully saved!'}); 
             }).catch(() =>  {
-                res.status(500).send({message: 'Internal error.' }); 
+                res.status(500).send( {message:'Internal error.'}); 
             }); 
 }
 
 exports.put = (req, res, next) =>  {
-    const id = req.params.id; 
-    res.status(200).send( {
-        id:id, 
-        item:req.body
-    }); 
-}
+    Product.findByIdAndUpdate(req.params.id, {
+        $set:{
+            title: req.body.title,
+            description: req.body.description,
+            slug: req.body.slug,
+            price: req.body.price
+        }
+        }).then( data => {
+            res.status(200).send({
+                message: 'your product was edited!'
+            });
+        }).catch(e =>{
+            res.status(400).send({
+                message:"An error ocurred in edition of your product!",
+                data: e
+            });
+        });
+};
 
 exports.delete = (req, res, next) =>  {
-    res.status(200).send(req.body); 
+    Product.findOneAndRemove(req.body.id)
+        .then( data => {
+            res.status(200).send({
+                message: 'your product was removed!'
+            });
+        }).catch(e =>{
+            res.status(400).send({
+                message:"An error ocurred in removed of your product!",
+                data: e
+            });
+        });
 }
 
 function valiteFieldsProduct (product) {
@@ -41,4 +99,4 @@ function valiteFieldsProduct (product) {
     }
             
     return false; 
-};
+}; 
--- src/routes/product-route.js
+++ src/routes/product-route.js
@@ -4,8 +4,11 @@ const express = require('express');
 const router = express.Router(); 
 const controller = require('../controllers/product-controller'); 
 
+router.get('/', controller.get);
+router.get('/:slug', controller.getBySlug); 
+router.get('/tags/:tags', controller.getByTags); 
 router.post('/', controller.post); 
 router.put('/:id', controller.put); 
-router.delete('', controller.delete); 
+router.delete('/', controller.delete); 
 
 module.exports = router; 
